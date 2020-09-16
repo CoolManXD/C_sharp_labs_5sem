2e8700c967cce2a42e0df08b074f5d9fbbf402e5
SerhiiYanchuk <novograd2201@gmail.com>
2020-09-16 23:37:05 +0300

    empty

---
+++ lab1(GenericCollection)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lab1_GenericCollection_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
